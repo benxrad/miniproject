8bde94c60f2bceac78aad12bf56e5fe80ea8fd97
Ben Salehirad <nsa163@sfu.ca>
2026-07-29 15:13:49 -0700

    up

---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+ 
+HOST, PORT = '', 9090
+ 
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print('Serving HTTP on port %s ...' % PORT)
+
+while True:
+    try:
+        client_connection, client_address = listen_socket.accept()
+        request = client_connection.recv(1024)
+        print(request)
+        if not request:
+            continue
+        filename = request.split()[1] #filename = /test.html
+        print("requested: ",filename.decode())
+        f = open(filename[1:],encoding="utf-8") #f = test.html
+        outputRequest = f.read()
+        print("FILE EXISTS - SEND 200;")
+        header = 'HTTP/1.1 200 OK\r\n\r\n'
+
+        client_connection.sendall(header.encode())
+
+        for i in range(0,len(outputRequest)):
+            client_connection.sendall(outputRequest[i].encode())
+        client_connection.close()
+    except IOError:
+        print("FILE DOES NOT EXIST - SEND 404;")
+        header = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
+        client_connection.sendall(header.encode())
+        client_connection.close()
+
+# http://localhost:8080/test.html
